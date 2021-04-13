3dbc57c24fc8c336e06137e42642c2132ee8a297
bertmad <skrivtilbertram@gmail.com>
2021-04-13 13:49:12 +0200

    Add function for printing error message

--- SLAM.sh
+++ SLAM.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# For when encountering problems
+error() {
+
+	echo "Error: $@" 1>&2
+	exit 1
+
+}
+
 # Function for checking whether it's booted as UEFI or BIOS
 checkBootMode() {
 
@@ -19,10 +27,10 @@ locateInstallDrive() {
 # Only used for debugging, will maybe remove
 main() {
 	echo "Refreshing keyrings..."
-	pacman --noconfirm -S archlinux-keyring 2> ErrorLog || { echo "Make sure to run this script as root, with an internet connection"; exit 1; }
-	pacman --noconfirm --needed -S dialog || { echo "Make sure to run this script as root, with an internet connection."; exit 1; }
+	pacman --noconfirm -S archlinux-keyring 2> ErrorLog || error "Make sure to run this script as root, with an internet connection."
+	pacman --noconfirm --needed -S dialog 2> ErrorLog || error "Make sure to run this script as root, with an internet connection."
 
-	dialog --title "LET'S GO!" --yesno "With refreshed keyrings and dialog installed we're are ready to take this script for a spin. Please, DO NOT run it unless you fully understand the risk! This was developed by me for me only, and as such there might be errors that worst case could wipe entire drives. You sure you want to continue?" 10 60 || { echo "User exited"; exit 1; }
+	dialog --title "LET'S GO!" --yesno "With refreshed keyrings and dialog installed we're are ready to take this script for a spin. Please, DO NOT run it unless you fully understand the risk! This was developed by me for me only, and as such there might be errors that worst case could wipe entire drives. You sure you want to continue?" 10 60 || error "User exited"
 
 	checkBootMode
 	locateInstallDrive
