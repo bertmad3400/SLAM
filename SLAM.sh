1564252a6c7833d328103861e979834425c08c67
bertmad <skrivtilbertram@gmail.com>
2021-04-20 11:36:37 +0200

    Change how initial installation works

--- SLAM.sh
+++ SLAM.sh
@@ -14,7 +14,8 @@ error(){
 cleanLogs
 
 echo "Refreshing keyrings..."
-pacman --noconfirm -S archlinux-keyring || error "Make sure to run this script as root, with an internet connection."
+# The reason for using -Sy is that there isn't space on the install disk for a full upgrade, but you need to update databases
+pacman --noconfirm -Sy archlinux-keyring || error "Make sure to run this script as root, with an internet connection."
 pacman --noconfirm --needed -S dialog || error "Make sure to run this script as root, with an internet connection."
 
 dialog --title "LET'S GO!" --yesno "With refreshed keyrings and dialog installed we're are ready to take this script for a spin. Please, DO NOT run it unless you fully understand the risk! This was developed by me for me only, and as such there might be errors that worst case could wipe entire drives. You sure you want to continue?" 10 60 || error "User exited"
