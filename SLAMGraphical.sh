7fff86873b693eeafd21a398f55ffef03b8b9339
bertmad3400 <skrivtilbertram@gmail.com>
2021-05-18 16:36:07 +0200

    Fixed function for installing software on laptops only

--- SLAMGraphical.sh
+++ SLAMGraphical.sh
@@ -184,7 +184,7 @@ chooseInstallMethod(){
 		M ) pacIn $package;;
 		A ) sudo -u "$username" yay -S --noconfirm "$package";;
 		G ) gitIn "$package" ;;
-		L ) [ "$deviceType" = "Laptop" ] && sudo -u "$username" yay -S --noconfirm $package;;
+		L* ) [ "$deviceType" = "Laptop" ] && chooseInstallMethod ${1:1};; # Calling the function for installing the software again, if the target is a laptop, but removes the first characther (the L) since it's verified that it's a laptop
 		D ) dialog --title "Dependencies" --infobox "Installing $package which is ${purpose}."; installPackages "${SLAMDir}/CSVFiles/$(echo "$package" | sed "s/^.*\///g")" ;;
 		* ) dialog --title "What??" --infobox "It seems that $package didn't have a tag, or it weren't recognized. Did you use the official files? If so please contact the developers. Skipping it for now" 0 0; echo "Error with following: \n package: $package \n tag: $tag \n purpose: $purpose \n" 1>> missingPackages; sleep 10 ;;
 	esac
